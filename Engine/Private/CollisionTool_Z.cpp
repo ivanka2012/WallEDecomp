09665f21c2a83f2ac014867921e0b6399a312019
ivanka2012 <ivanka2012@gmail.com>
2024-12-11 23:17:03 +0100

    Buzzwords don't matter. I think I should include the ODE somehow...

--- Engine/Private/CollisionTool_Z.cpp
+++ Engine/Private/CollisionTool_Z.cpp
@@ -54,6 +54,7 @@ SegmentVsCylindre(Segment_Z const&,Cylindre_Z const&,CollisionReport_Z &)	__text
 #include <Types_Z.h>
 #include <Math_Z.h>
 #include <math.h>
+#include "odemath.h"
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
@@ -451,11 +452,12 @@ Bool MovingSphereVsQuad(const Capsule_Z &Cap, const Vec4f &p1, const Vec4f &p2, 
 	return TRUE;
 }
 
-// This looks vaguely similar to the cldTestEdge found in OpenDynamicsEngine but there are some important changes in this version, notably the introduction of epsilons.
 
 Float fBestDepth;
 Vec4f vBestNormal;
 int iBestAxis;
+
+// The following few subroutines look vaguely similar to the ones found in ~2006 in OpenDynamicsEngine but there are some important changes in this version, notably the introduction of epsilons.
 
 static Bool _cldTestEdge(Float fp0, Float fp1, Float fR,
 						 const Float *vNormal, int iAxis)
@@ -562,6 +564,8 @@ const float &MaxRef(const float &a, const float &b)
 /**
  * TODO: figure out the register spill and the mixed arguments in minimum selection
  */
+
+// Test box axis as separating axis 
 static Bool _cldTestFace(Float fp0, Float fp1, Float fp2, Float fR,
 						 const Float *vNormal, int iAxis)
 {
@@ -572,49 +576,39 @@ static Bool _cldTestFace(Float fp0, Float fp1, Float fp2, Float fR,
 	fMin = MinRef(fp2, MinRef(fp0, fp1));
 	fMax = MaxRef(fp2, MaxRef(fp0, fp1));
 
-	// check if we overlapp
 	Float fDepthMin = fR - fMin;
 	Float fDepthMax = fMax + fR;
 
 	// ASOBO: Take the multiplication and see if that's close enough to zero. Saved some branches!
+	// if we dont't have overlapping interval
 	if ((fDepthMin * fDepthMax) < 1e-10f)
 	{
 		// do nothing
 		return FALSE;
 	}
 
-	// calculate normal's length
-	// This pointer cast is dirty as hell, but it's what Asobo does here (or something close to it).
-	// Float fLength = Vec4_GetNorm(*(Vec4f *)(vNormal));
-
 	if (fDepthMin > fDepthMax)
 	{
-		// if lower depth than best found so far (favor face over edges)
 		if (fDepthMax < fBestDepth)
 		{
-			// remember current axis as best axis
 			// How the fuck does negating these numbers result in a stack allocation for me but not for Asobo???
 			vBestNormal[0] = -vNormal[0];
 			vBestNormal[1] = -vNormal[1];
 			vBestNormal[2] = -vNormal[2];
 			vBestNormal[3] = -vNormal[3];
 			iBestAxis = iAxis;
-			// dAASSERT(fDepth>=0);
 			fBestDepth = fDepthMax;
 		}
 	}
 	else
 	{
-		// if lower depth than best found so far (favor face over edges)
 		if (fDepthMin < fBestDepth)
 		{
-			// remember current axis as best axis
 			vBestNormal[0] = vNormal[0];
 			vBestNormal[1] = vNormal[1];
 			vBestNormal[2] = vNormal[2];
 			vBestNormal[3] = vNormal[3];
 			iBestAxis = iAxis;
-			// dAASSERT(fDepth>=0);
 			fBestDepth = fDepthMin;
 		}
 	}
@@ -655,9 +649,18 @@ void dLineClosestApproach(const Float *pa, const Float *ua,
  * Oh thank god 
  * Nothing to see here! Moving on...
  */
+
+// given n points in the plane (array p, of size 2*n), generate m points that
+// best represent the whole set. the definition of 'best' here is not
+// predetermined - the idea is to select points that give good box-box
+// collision detection behavior. the chosen point indexes are returned in the
+// array iret (of size m). 'i0' is always the first entry in the array.
+// n must be in the range [1..8]. m must be in the range [1..n]. i0 must be
+// in the range [0..n-1].
+
 void cullPoints(int n, Float p[], int m, int i0, int iret[])
 {
-
+	// compute the centroid of the polygon in cx,cy
 	int i, j;
 	Float a, cx, cy, q;
 	if (n == 1)
@@ -688,10 +691,12 @@ void cullPoints(int n, Float p[], int m, int i0, int iret[])
 		cy = a * (cy + q * (p[n * 2 - 1] + p[1]));
 	}
 
+	// compute the angle of each point w.r.t. the centroid
 	Float A[8];
 	for (i = 0; i < n; i++)
 		A[i] = Atan2(p[i * 2 + 1] - cy, p[i * 2] - cx);
 
+	// search for points that have angles closest to A[i0] + i*(2*pi/m).
 	int avail[8];
 	for (i = 0; i < n; i++)
 		avail[i] = 1;
@@ -725,3 +730,390 @@ void cullPoints(int n, Float p[], int m, int i0, int iret[])
 	}
 }
 
+struct Box_Z{
+	//Standard issue translation-rotation-scaling matrix...
+	Mat4x4 transform;
+};
+
+struct FastBox_Z{
+	Mat4x4 rotMat; //+0x00
+	Mat4x4 rotMatAgain; //+0x40
+	Vec4f translation; //+0x80
+	Vec4f scale; //+0x90
+	Float radius; //+0xA0 The radius of the sphere surrounding this box.
+};
+
+struct FastBoxVsCol{
+	//Has some FastBox_Z* pointers...
+
+
+	FastBox_Z* unk1; //+0x10
+	FastBox_Z* unk2; //+0x14
+};
+
+
+
+int FastBoxVsFastBox3(FastBoxVsCol* col)
+{
+  /*const Float fudge_factor = Float(1.05);
+  Float* p,pp,normalC;
+  const Float *normalR = 0;
+  Float A[3],B[3],R11,R12,R13,R21,R22,R23,R31,R32,R33,
+    Q11,Q12,Q13,Q21,Q22,Q23,Q31,Q32,Q33,s,s2,l,expr1_val;
+  int i,j,invert_normal,code;
+
+  // get vector from centers of box 1 to box 2, relative to box 1
+  p[0] = p2[0] - p1[0];
+  p[1] = p2[1] - p1[1];
+  p[2] = p2[2] - p1[2];
+  dMULTIPLY1_331 (pp,R1,p);		// get pp = p relative to body 1
+
+  // get side lengths / 2
+  A[0] = side1[0]*Float(0.5);
+  A[1] = side1[1]*Float(0.5);
+  A[2] = side1[2]*Float(0.5);
+  B[0] = side2[0]*Float(0.5);
+  B[1] = side2[1]*Float(0.5);
+  B[2] = side2[2]*Float(0.5);
+
+  // Rij is R1'*R2, i.e. the relative rotation between R1 and R2
+  R11 = dDOT44(R1+0,R2+0); R12 = dDOT44(R1+0,R2+1); R13 = dDOT44(R1+0,R2+2);
+  R21 = dDOT44(R1+1,R2+0); R22 = dDOT44(R1+1,R2+1); R23 = dDOT44(R1+1,R2+2);
+  R31 = dDOT44(R1+2,R2+0); R32 = dDOT44(R1+2,R2+1); R33 = dDOT44(R1+2,R2+2);
+
+  Q11 = dFabs(R11); Q12 = dFabs(R12); Q13 = dFabs(R13);
+  Q21 = dFabs(R21); Q22 = dFabs(R22); Q23 = dFabs(R23);
+  Q31 = dFabs(R31); Q32 = dFabs(R32); Q33 = dFabs(R33);*/
+
+  // for all 15 possible separating axes:
+  //   * see if the axis separates the boxes. if so, return 0.
+  //   * find the depth of the penetration along the separating axis (s2)
+  //   * if this is the largest depth so far, record it.
+  // the normal vector will be set to the separating axis with the smallest
+  // depth. note: normalR is set to point to a column of R1 or R2 if that is
+  // the smallest depth normal so far. otherwise normalR is 0 and normalC is
+  // set to a vector relative to body 1. invert_normal is 1 if the sign of
+  // the normal should be flipped.
+
+  /*do {
+#define TST(expr1,expr2,norm,cc) \
+    expr1_val = (expr1); \
+    s2 = Abs(expr1_val) - (expr2); \
+    if (s2 > 0) return 0; \
+    if (s2 > s) { \
+      s = s2; \
+      normalR = norm; \
+      invert_normal = ((expr1_val) < 0); \
+      code = (cc); \
+	  if (flags & CONTACTS_UNIMPORTANT) break; \
+	}
+
+    s = -dInfinity;
+    invert_normal = 0;
+    code = 0;
+
+    // separating axis = u1,u2,u3
+    TST (pp[0],(A[0] + B[0]*Q11 + B[1]*Q12 + B[2]*Q13),R1+0,1);
+    TST (pp[1],(A[1] + B[0]*Q21 + B[1]*Q22 + B[2]*Q23),R1+1,2);
+    TST (pp[2],(A[2] + B[0]*Q31 + B[1]*Q32 + B[2]*Q33),R1+2,3);
+
+    // separating axis = v1,v2,v3
+    TST (dDOT41(R2+0,p),(A[0]*Q11 + A[1]*Q21 + A[2]*Q31 + B[0]),R2+0,4);
+    TST (dDOT41(R2+1,p),(A[0]*Q12 + A[1]*Q22 + A[2]*Q32 + B[1]),R2+1,5);
+    TST (dDOT41(R2+2,p),(A[0]*Q13 + A[1]*Q23 + A[2]*Q33 + B[2]),R2+2,6);
+
+    // note: cross product axes need to be scaled when s is computed.
+    // normal (n1,n2,n3) is relative to box 1.
+#undef TST
+#define TST(expr1,expr2,n1,n2,n3,cc) \
+    expr1_val = (expr1); \
+    s2 = Abs(expr1_val) - (expr2); \
+    if (s2 > 0.001f) return 0; \
+    l = Sqrt ((n1)*(n1) + (n2)*(n2) + (n3)*(n3)); \
+    if (l > 0) { \
+      s2 /= l; \
+      if (s2*fudge_factor > s) { \
+        s = s2; \
+        normalR = 0; \
+        normalC[0] = (n1)/l; normalC[1] = (n2)/l; normalC[2] = (n3)/l; \
+        invert_normal = ((expr1_val) < 0); \
+        code = (cc); \
+        if (flags & CONTACTS_UNIMPORTANT) break; \
+	  } \
+	}
+
+    // separating axis = u1 x (v1,v2,v3)
+    TST(pp[2]*R21-pp[1]*R31,(A[1]*Q31+A[2]*Q21+B[1]*Q13+B[2]*Q12),0,-R31,R21,7);
+    TST(pp[2]*R22-pp[1]*R32,(A[1]*Q32+A[2]*Q22+B[0]*Q13+B[2]*Q11),0,-R32,R22,8);
+    TST(pp[2]*R23-pp[1]*R33,(A[1]*Q33+A[2]*Q23+B[0]*Q12+B[1]*Q11),0,-R33,R23,9);
+
+    // separating axis = u2 x (v1,v2,v3)
+    TST(pp[0]*R31-pp[2]*R11,(A[0]*Q31+A[2]*Q11+B[1]*Q23+B[2]*Q22),R31,0,-R11,10);
+    TST(pp[0]*R32-pp[2]*R12,(A[0]*Q32+A[2]*Q12+B[0]*Q23+B[2]*Q21),R32,0,-R12,11);
+    TST(pp[0]*R33-pp[2]*R13,(A[0]*Q33+A[2]*Q13+B[0]*Q22+B[1]*Q21),R33,0,-R13,12);
+
+    // separating axis = u3 x (v1,v2,v3)
+    TST(pp[1]*R11-pp[0]*R21,(A[0]*Q21+A[1]*Q11+B[1]*Q33+B[2]*Q32),-R21,R11,0,13);
+    TST(pp[1]*R12-pp[0]*R22,(A[0]*Q22+A[1]*Q12+B[0]*Q33+B[2]*Q31),-R22,R12,0,14);
+    TST(pp[1]*R13-pp[0]*R23,(A[0]*Q23+A[1]*Q13+B[0]*Q32+B[1]*Q31),-R23,R13,0,15);
+#undef TST
+  } while (0);
+
+  if (!code) return 0;
+
+  // if we get to this point, the boxes interpenetrate. compute the normal
+  // in global coordinates.
+  if (normalR) {
+    normal[0] = normalR[0];
+    normal[1] = normalR[4];
+    normal[2] = normalR[8];
+  }
+  else {
+    dMULTIPLY0_331 (normal,R1,normalC);
+  }
+  if (invert_normal) {
+    normal[0] = -normal[0];
+    normal[1] = -normal[1];
+    normal[2] = -normal[2];
+  }
+  *depth = -s;
+
+  // compute contact point(s)
+
+  if (code > 6) {
+    // an edge from box 1 touches an edge from box 2.
+    // find a point pa on the intersecting edge of box 1
+    Float* pa;
+    Float sign;
+    for (i=0; i<3; i++) pa[i] = p1[i];
+    for (j=0; j<3; j++) {
+      sign = (dDOT14(normal,R1+j) > 0) ? Float(1.0) : Float(-1.0);
+      for (i=0; i<3; i++) pa[i] += sign * A[j] * R1[i*4+j];
+    }
+
+    // find a point pb on the intersecting edge of box 2
+    Float* pb;
+    for (i=0; i<3; i++) pb[i] = p2[i];
+    for (j=0; j<3; j++) {
+      sign = (dDOT14(normal,R2+j) > 0) ? Float(-1.0) : Float(1.0);
+      for (i=0; i<3; i++) pb[i] += sign * B[j] * R2[i*4+j];
+    }
+
+    Float alpha,beta;
+    Float* ua,ub;
+    for (i=0; i<3; i++) ua[i] = R1[((code)-7)/3 + i*4];
+    for (i=0; i<3; i++) ub[i] = R2[((code)-7)%3 + i*4];
+
+    dLineClosestApproach (pa,ua,pb,ub,&alpha,&beta);
+    for (i=0; i<3; i++) pa[i] += ua[i]*alpha;
+    for (i=0; i<3; i++) pb[i] += ub[i]*beta;
+
+    for (i=0; i<3; i++) contact[0].pos[i] = Float(0.5)*(pa[i]+pb[i]);
+    contact[0].depth = *depth;
+    *return_code = code;
+    return 1;
+  }
+
+  // okay, we have a face-something intersection (because the separating
+  // axis is perpendicular to a face). define face 'a' to be the reference
+  // face (i.e. the normal vector is perpendicular to this) and face 'b' to be
+  // the incident face (the closest face of the other box).
+
+  const Float *Ra,*Rb,*pa,*pb,*Sa,*Sb;
+  if (code <= 3) {
+    Ra = R1;
+    Rb = R2;
+    pa = p1;
+    pb = p2;
+    Sa = A;
+    Sb = B;
+  }
+  else {
+    Ra = R2;
+    Rb = R1;
+    pa = p2;
+    pb = p1;
+    Sa = B;
+    Sb = A;
+  }
+
+  // nr = normal vector of reference face dotted with axes of incident box.
+  // anr = absolute values of nr.
+  Float* normal2,nr,anr;
+  if (code <= 3) {
+    normal2[0] = normal[0];
+    normal2[1] = normal[1];
+    normal2[2] = normal[2];
+  }
+  else {
+    normal2[0] = -normal[0];
+    normal2[1] = -normal[1];
+    normal2[2] = -normal[2];
+  }
+  dMULTIPLY1_331 (nr,Rb,normal2);
+  anr[0] = dFabs (nr[0]);
+  anr[1] = dFabs (nr[1]);
+  anr[2] = dFabs (nr[2]);
+
+  // find the largest compontent of anr: this corresponds to the normal
+  // for the indident face. the other axis numbers of the indicent face
+  // are stored in a1,a2.
+  int lanr,a1,a2;
+  if (anr[1] > anr[0]) {
+    if (anr[1] > anr[2]) {
+      a1 = 0;
+      lanr = 1;
+      a2 = 2;
+    }
+    else {
+      a1 = 0;
+      a2 = 1;
+      lanr = 2;
+    }
+  }
+  else {
+    if (anr[0] > anr[2]) {
+      lanr = 0;
+      a1 = 1;
+      a2 = 2;
+    }
+    else {
+      a1 = 0;
+      a2 = 1;
+      lanr = 2;
+    }
+  }
+
+  // compute center point of incident face, in reference-face coordinates
+  Float* center;
+  if (nr[lanr] < 0) {
+    for (i=0; i<3; i++) center[i] = pb[i] - pa[i] + Sb[lanr] * Rb[i*4+lanr];
+  }
+  else {
+    for (i=0; i<3; i++) center[i] = pb[i] - pa[i] - Sb[lanr] * Rb[i*4+lanr];
+  }
+
+  // find the normal and non-normal axis numbers of the reference box
+  int codeN,code1,code2;
+  if (code <= 3) codeN = code-1; else codeN = code-4;
+  if (codeN==0) {
+    code1 = 1;
+    code2 = 2;
+  }
+  else if (codeN==1) {
+    code1 = 0;
+    code2 = 2;
+  }
+  else {
+    code1 = 0;
+    code2 = 1;
+  }
+
+  // find the four corners of the incident face, in reference-face coordinates
+  Float quad[8];	// 2D coordinate of incident face (x,y pairs)
+  Float c1,c2,m11,m12,m21,m22;
+  c1 = dDOT14 (center,Ra+code1);
+  c2 = dDOT14 (center,Ra+code2);
+  // optimize this? - we have already computed this data above, but it is not
+  // stored in an easy-to-index format. for now it's quicker just to recompute
+  // the four dot products.
+  m11 = dDOT44 (Ra+code1,Rb+a1);
+  m12 = dDOT44 (Ra+code1,Rb+a2);
+  m21 = dDOT44 (Ra+code2,Rb+a1);
+  m22 = dDOT44 (Ra+code2,Rb+a2);
+  {
+    Float k1 = m11*Sb[a1];
+    Float k2 = m21*Sb[a1];
+    Float k3 = m12*Sb[a2];
+    Float k4 = m22*Sb[a2];
+    quad[0] = c1 - k1 - k3;
+    quad[1] = c2 - k2 - k4;
+    quad[2] = c1 - k1 + k3;
+    quad[3] = c2 - k2 + k4;
+    quad[4] = c1 + k1 + k3;
+    quad[5] = c2 + k2 + k4;
+    quad[6] = c1 + k1 - k3;
+    quad[7] = c2 + k2 - k4;
+  }
+
+  // find the size of the reference face
+  Float rect[2];
+  rect[0] = Sa[code1];
+  rect[1] = Sa[code2];
+
+  // intersect the incident and reference faces
+  Float ret[16];
+  int n = intersectRectQuad (rect,quad,ret);
+  if (n < 1) return 0;		// this should never happen
+
+  // convert the intersection points into reference-face coordinates,
+  // and compute the contact position and depth for each point. only keep
+  // those points that have a positive (penetrating) depth. delete points in
+  // the 'ret' array as necessary so that 'point' and 'ret' correspond.
+  Float point[3*8];		// penetrating contact points
+  Float dep[8];			// depths for those points
+  Float det1 = dRecip(m11*m22 - m12*m21);
+  m11 *= det1;
+  m12 *= det1;
+  m21 *= det1;
+  m22 *= det1;
+  int cnum = 0;			// number of penetrating contact points found
+  for (j=0; j < n; j++) {
+    Float k1 =  m22*(ret[j*2]-c1) - m12*(ret[j*2+1]-c2);
+    Float k2 = -m21*(ret[j*2]-c1) + m11*(ret[j*2+1]-c2);
+    for (i=0; i<3; i++) point[cnum*3+i] =
+			  center[i] + k1*Rb[i*4+a1] + k2*Rb[i*4+a2];
+    dep[cnum] = Sa[codeN] - dDOT(normal2,point+cnum*3);
+    if (dep[cnum] >= 0) {
+      ret[cnum*2] = ret[j*2];
+      ret[cnum*2+1] = ret[j*2+1];
+      cnum++;
+	  if ((cnum | CONTACTS_UNIMPORTANT) == (flags & (NUMC_MASK | CONTACTS_UNIMPORTANT))) {
+		  break;
+	  }
+    }
+  }
+  if (cnum < 1) { 
+	  return 0;	// this should not happen, yet does at times (demo_plane2d single precision).
+  }
+
+  // we can't generate more contacts than we actually have
+  int maxc = flags & NUMC_MASK;
+  if (maxc > cnum) maxc = cnum;
+  if (maxc < 1) maxc = 1;	// Even though max count must not be zero this check is kept for backward compatibility as this is a public function
+
+  if (cnum <= maxc) {
+    // we have less contacts than we need, so we use them all
+    for (j=0; j < cnum; j++) {
+      dContactGeom *con = CONTACT(contact,skip*j);
+      for (i=0; i<3; i++) con->pos[i] = point[j*3+i] + pa[i];
+      con->depth = dep[j];
+    }
+  }
+  else {
+    dIASSERT(!(flags & CONTACTS_UNIMPORTANT)); // cnum should be generated not greater than maxc so that "then" clause is executed
+    // we have more contacts than are wanted, some of them must be culled.
+    // find the deepest point, it is always the first contact.
+    int i1 = 0;
+    Float maxdepth = dep[0];
+    for (i=1; i<cnum; i++) {
+      if (dep[i] > maxdepth) {
+	maxdepth = dep[i];
+	i1 = i;
+      }
+    }
+
+    int iret[8];
+    cullPoints (cnum,ret,maxc,i1,iret);
+
+    for (j=0; j < maxc; j++) {
+      dContactGeom *con = CONTACT(contact,skip*j);
+      for (i=0; i<3; i++) con->pos[i] = point[iret[j]*3+i] + pa[i];
+      con->depth = dep[iret[j]];
+    }
+    cnum = maxc;
+  }
+
+  *return_code = code;
+  return cnum;*/
+}
